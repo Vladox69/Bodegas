77478b4cdbfd10c37e7b447923c5c0c28f55e1c2
Vladox69 <marwladiagua2001@hotmail.com>
2021-07-01 16:34:12 -0500

    Creación del proyecto

---
+++ views/modules/contacto.php
@@ -0,0 +1,34 @@
+<main class="contenedor">
+        <h3 class="centrar-texto">Contacto</h3>
+        <div class="contacto-background"></div>
+        <form class="formulario">
+            <div class="campo">
+                <label class="campo__label" for="nombre">Nombre</label>
+                <input class="campo__field"
+                type="text" 
+                placeholder="Tu nombre" 
+                id="nombre">
+            </div><!--.campo-->
+
+            <div class="campo">
+                <label class="campo__label" for="e-mail">E-mail</label>
+                <input class="campo__field"
+                type="email" 
+                placeholder="Tu E-mail" 
+                id="e-mail">
+            </div><!--.campo-->
+
+            <div class="campo">
+                <label class="campo__label" for="mensaje">Mensaje</label>
+                <textarea class="campo__field campo__field--textarea"
+                id="mensaje">
+                </textarea>
+            </div><!--.campo-->
+
+            <div class="campo">
+                <input type="submit" 
+                value="Enviar" 
+                class="boton boton--primario">
+            </div><!--campo-->
+                
+        </form>
